f484c0830c94dd98a133526cb2571b1996d3130e
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-06-23 12:12:23 +0100

    Working RPS

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,3 +1,9 @@
+pairs = {
+	"rock": { "beats": "scissors"},
+	"paper": { "beats": "rock" },
+	"scissors": { "beats": "paper" }
+}
+
 function Player(name) {
   this.name = name;
 };
@@ -11,8 +17,22 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
-Game.prototype.winner = function() {
-  if(1 === 1) {
-    return this.player1;
-  }
+Game.prototype.samePick = function() {
+	return this.player1.pick === this.player2.pick;
 };
+
+Game.prototype.winner = function() {
+	if (this.samePick() === true) {
+		return null;
+	}
+	else if (pairs[this.player1.pick]["beats"] === this.player2.pick) {
+		return this.player1;
+	}
+	else {
+		return this.player2;
+	}
+};
+
+
+
+
